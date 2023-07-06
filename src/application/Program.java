d0351975e0e108c4bcc8a6aa05f0c0ab914dd697
Rodrigo Oliveira <rodrigo.olv.santos@gmail.com>
2023-07-05 21:20:29 -0300

    Working reading files using clusule files

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,23 +10,36 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		File file = new File ("C:\\Arquivos\\in.txt");
-		Scanner sc = null;
+		String path = "C:\\Arquivos\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
+		
 		try {
-			sc = new Scanner (file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(br);
+			
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
-		}
-		catch (IOException e) {
-			System.out.println("Error: " + e);
+		}catch (IOException e) {
+			System.out.println("Error" +e.getMessage());
 		}
 		finally {
-			if (sc !=null) {
-				sc.close();	
+			try {
+			if (br != null) {
+				br.close();
 			}
-			
+			if (fr != null) {
+				fr.close();
+			}
 		}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 	}
 
+}
 }
